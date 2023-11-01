bf68c5505e7fe846d9f0376ed899f246526ce623
Federico Peregrin <1678federico@gmail.com>
2023-10-31 22:30:32 -0300

    Portfolio Website

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,46 @@
+import { useState } from "react";
+
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!formData.name || !formData.email || !formData.message) {
+      alert(
+        "Por favor, complete todos los campos antes de enviar el formulario."
+      );
+      return;
+    }
+
+    // Si todos los campos están llenos, puedes enviar el formulario a través del atributo "action"
+    const form = e.target;
+    form.submit();
+
+    // Restablecer el estado del mensaje después del envío
+    setFormData({ name: "", email: "", message: "" });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
   return (
     <div
       name="contact"
       className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4"
     >
-      <form action="" className="flex flex-col max-w-[600px] w-full">
+      <form
+        method="POST"
+        action="https://getform.io/f/4081f3f8-54e6-4bee-85c7-566179622e2f"
+        className="flex flex-col max-w-[600px] w-full"
+        onSubmit={handleSubmit}
+      >
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
             Contact
@@ -18,18 +54,24 @@ const Contact = () => {
           type="text"
           placeholder="Name"
           name="name"
+          value={formData.name}
+          onChange={handleChange}
         />
         <input
           className="my-4 p-2 bg-[#ccd6f6]"
           type="email"
           placeholder="Email"
           name="email"
+          value={formData.email}
+          onChange={handleChange}
         />
         <textarea
           className="bg-[#ccd6f6] p-2"
           name="message"
           rows="10"
           placeholder="Message"
+          value={formData.message}
+          onChange={handleChange}
         ></textarea>
         <button className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center">
           Let's Collaborate
---
+++ src/data/data.js
@@ -0,0 +1,26 @@
+import WorkIm from "../assets/projects/workImg.jpeg";
+import RealEst from "../assets/projects/realestate.jpg";
+
+export const data = [
+  {
+    id: 1,
+    name: "React JS Application",
+    image: WorkIm,
+    github: "https://github.com/fede1678/movies-react",
+    live: "https://pagina-de-prueba2.netlify.app/",
+  },
+  {
+    id: 2,
+    name: "React JS Application",
+    image: RealEst,
+    github: "",
+    live: "https://lola-peliculas.netlify.app/",
+  },
+  {
+    id: 3,
+    name: "React JS Application",
+    image: WorkIm,
+    github: "",
+    live: "",
+  },
+];
