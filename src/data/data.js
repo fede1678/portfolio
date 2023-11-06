662886c123e8faf70d6a5981912cf7af93d35df1
Federico Peregrin <1678federico@gmail.com>
2023-11-06 09:09:02 -0300

    Portfolio Website

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,9 @@
 import WorkIm1 from "../assets/projects/workImg.png";
 import WorkIm2 from "../assets/projects/pelis.png";
+import WorkIm3 from "../assets/projects/portafolio.png";
+import WorkIm4 from "../assets/projects/tienda.png";
+import WorkIm5 from "../assets/projects/workImg.jpeg";
+import WorkIm6 from "../assets/projects/realestate.jpg";
 
 export const data = [
   {
@@ -15,5 +19,33 @@ export const data = [
     image: WorkIm2,
     github: "https://github.com/fede1678/movies-react",
     live: "https://lola-peliculas.netlify.app/",
+  },
+  {
+    id: 3,
+    name: "React JS Application",
+    image: WorkIm4,
+    github: "https://github.com/fede1678/dashboard-store",
+    live: "https://inspiring-jelly-5385cd.netlify.app/",
+  },
+  {
+    id: 4,
+    name: "React JS Application",
+    image: WorkIm3,
+    github: "https://github.com/fede1678/portfolio",
+    live: "https://jolly-malabi-de81f3.netlify.app/",
+  },
+  {
+    id: 5,
+    name: "React JS Application",
+    image: WorkIm5,
+    github: "https://github.com/fede1678/movies-react",
+    live: "https://lola-peliculas.netlify.app/",
+  },
+  {
+    id: 6,
+    name: "React JS Application",
+    image: WorkIm6,
+    github: "https://github.com/fede1678/portfolio",
+    live: "https://jolly-malabi-de81f3.netlify.app/",
   },
 ];
