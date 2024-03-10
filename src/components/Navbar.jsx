55ec444310eeaa3463b2694f9fa20949dd745118
Federico Peregrin <1678federico@gmail.com>
2024-03-10 02:11:54 -0300

    portafolio-website

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -154,7 +154,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300  bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="https://drive.google.com/file/d/1vLCLGjlgcST_6pg882cSxOS19N2ioeUF/view?usp=sharing"
+              href="https://drive.google.com/file/d/1Frp3B8yCxVC_iAuXIcJA8xpBCctrJlkz/view?usp=sharing"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
